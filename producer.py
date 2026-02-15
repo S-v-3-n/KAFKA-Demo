7e5c1702d129aa6e47811c0d73a3a882b63453f1
S-v-3-n <sven.mrs.n@gmail.com>
2026-02-15 12:33:56 +0100

    init producer

---
+++ producer.py
@@ -0,0 +1,37 @@
+import json
+import uuid
+from confluent_kafka import Producer
+
+producer_config = {
+    'bootstrap.servers': 'localhost:9092'
+}
+
+producer = Producer(producer_config)
+
+order = {
+    'order_id': str(uuid.uuid4()),
+    'user': 'Robert',
+    'item': 'mushroom_pizza',
+    'quantity': 2
+}
+
+value = json.dumps(order).encode('utf-8')
+
+def delivery_report(err, msg):
+    if err:
+        print(f'❌ Delivery failed : {err}')
+    else:
+        print(f'✅ Delivered {msg.value().decode("utf-8")}')
+        print(f'✅ Delivered {msg.topic()} : partition {msg.partition()} : at offset {msg.offset()}')
+
+
+producer.produce(
+    topic='orders',
+    value=value,
+    callback=delivery_report
+)
+# Force to send before ending
+producer.flush()
+
+
+
